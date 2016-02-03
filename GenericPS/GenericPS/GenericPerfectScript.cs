578e950b4f4df53e914cfc509512db33d6411375
ricgaudet <ricgaudet@gmail.com>
2016-02-03 11:44:37 -0700

    Incorporated various dlls as projects for clarity

---
+++ GenericPS/GenericPS/GenericPerfectScript.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPS
+{
+    public abstract class GenericPerfectScript
+    {
+        public dynamic perfectScript { get; set; }
+        public int version { get; set; }
+    }
+}
---
+++ WP14/WP14/WP14PS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GenericPS;
+using WordPerfect;
+
+namespace WP14
+{
+    public class WP14PS : GenericPerfectScript
+    {
+        public WP14PS()
+        {
+            perfectScript = new PerfectScript();
+            version = 14;
+        }
+
+    }
+}
