6d4dd22092c06373efaec48eef6e9609b9444af6
ric.gaudet <ric.gaudet@gmail.com>
2018-11-14 15:58:54 -0700

    added wp 18

---
+++ WP18/WP18/WP18PS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GenericPS;
+using WordPerfect;
+//using PerfectScript;
+//using PerfectFit;
+
+
+namespace WP18
+{
+    public class WP18PS: GenericPerfectScript
+    {
+        public WP18PS()
+        {
+
+            //script = new PerfectScript.Script();
+            perfectScript = new WordPerfect.PerfectScript();
+           // perfectFit = new PerfectFit.PerfectScript();
+            version = 18;
+            
+  
+        }
+
+    }
+}
